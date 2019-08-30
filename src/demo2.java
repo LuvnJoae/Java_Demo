5382dc7a9c1fa4da0e0fb89beba943767b0c1fb6
luvnjoae <lichang0637@163.com>
2019-08-30 20:02:17 +0800

    add

--- src/demo2.java
+++ src/demo2.java
@@ -2,5 +2,6 @@ public class demo2 {
     public static void main(String[] args) {
         System.out.println("test2");
         System.out.println("123");
+        System.out.println("123");
     }
 }
